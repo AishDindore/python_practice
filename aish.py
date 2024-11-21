139cd2235a9ff3a1b08d63e43bb6a4853e9f12a1
python_practice <aishwarya.dindore07@gmail.com>
2024-11-21 17:10:43 +0530

    add initial file

---
+++ aish.py
@@ -0,0 +1,9 @@
+# append()
+fruits = ["apple","mango","banana","orange"]
+fruits.append("papaya")
+print(fruits)
+
+# program 2
+#             0        1       2      3
+animals = ["rabbit","snake","tiger","lion"]
+print(animals[0])
