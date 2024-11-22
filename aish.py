fefe1103d1fe72f73b20aada15391fa0ab8ecede
python_practice <aishwarya.dindore07@gmail.com>
2024-11-22 11:51:49 +0530

    check

--- aish.py
+++ aish.py
@@ -1,9 +1,1 @@
-# append()
-fruits = ["apple","mango","banana","orange"]
-fruits.append("papaya")
-print(fruits)
-
-# program 2
-#             0        1       2      3
-animals = ["rabbit","snake","tiger","lion"]
-print(animals[0])
+print("this is a new feature")
